e361ab9421e4df239c3dd31842a87810fac35ee7
Darlight <andres.perdomo09@gmail.com>
2019-04-05 07:42:51 -0600

    Heap y PriorityQueue Creados

---
+++ PriorityQueue.java
@@ -0,0 +1,22 @@
+//Extraido de:  https://docs.oracle.com/javase/7/docs/api/java/util/PriorityQueue.html
+public class PriorityQueue extends AbstractQueue{
+    public PriorityQueue(){
+
+    }
+    public PriorityQueue(Collection<? extends E> c){
+
+    }
+    public PriorityQueue(int initialCapacity){
+
+    }
+    public PriorityQueue(int initialCapacity, Comparator<? super E> comparator){
+
+    }
+    public PriorityQueue(PriorityQueue<? extends E> c){
+
+    }
+    public PriorityQueue(SortedSet<? extends E> c){
+
+    }
+
+}
